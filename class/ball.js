fe988c46a9698ce43a8f9e42c65aeb53feeeb939
Nicolas <arce.nico10@gmail.com>
2023-03-20 21:08:32 -0600

    Ball now works

--- class/ball.js
+++ class/ball.js
@@ -1,10 +1,20 @@
 class Ball{
-    constructor(x,y,radius,id){
-        this.sprite=new Sprite(x,y,radius,'kinematic')
+    constructor(x,y,radius){
+        balls.push(this)
+        this.sprite=new Sprite(x,y,radius)
         this.sprite.debug=true
-        this.id=id
+        this.id=undefined
+        let ids=[]
+        balls.forEach(ball=>{
+            ids.push(ball.id)
+        })
+        let tempId=1
+        for(tempId;ids.includes(tempId);tempId=Math.floor(random(1,1001))){console.log('Id exists:'+tempId)}
+        this.id=tempId
+        console.log(tempId)
+        this.sprite.bounciness=1
         this.rad=radius
-        this.orig=[x,y]
+        this.orig={x:x,y:y}
         let pixels=`
 ..11
 .1111
@@ -14,12 +24,29 @@ class Ball{
 ..11`
         let palette={'1':color(255,255,255)}
         this.sprite.img=spriteArt(pixels,round(radius)/6,palette)
+        balls.forEach(ball=>{
+                this.sprite.overlaps(ball.sprite)
+        })
     }
     draw(){
+        if(this.removed==true){
+            this.sprite.remove()
+            balls.splice(balls.indexOf(this),1)
+        }
         if(gameState==0){
             this.sprite.x=paddle.sprite.x
-            this.sprite.y=this.orig[1]
+            this.sprite.y=this.orig.y
         }
+        this.sprite.rotation=0
+        if(this.sprite.y>height){
+            this.removed=true
+        }
+        if(this.sprite.collides(edges)){
+            let temp=random(sounds.ball_hit)
+            temp.play()
+        }
+        /*fill(255)
+        text(this.id,this.sprite.x,this.sprite.y+this.rad/2,15)*/
     }
     start(){
         this.sprite.velocity.x=Math.floor(random(-4,5))
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var balls=[], blocks_padded=[], paddle, modifiers=[], sounds={modifier_get:[], ball_hit:[], game_over:[]}, gameState=0,edges=[]
+var balls=[], blocks_padded=[], paddle, modifiers=[], sounds={modifier_get:[], ball_hit:[], game_over:[]}, gameState=0,edges
 var longSide
 var preload=()=>{
     sounds.modifier_get.push(loadSound('sounds/modifier_get_good.wav'), loadSound('sounds/modifier_get_bad.wav'))
@@ -10,16 +10,17 @@ var preload=()=>{
 var setup=()=>{
     new Canvas(innerWidth, innerHeight)
     longSide=width>height?width:height
-    balls.push(new Ball(innerWidth/2, innerHeight-(innerHeight/10),longSide/30))
+    new Ball(innerWidth/2, innerHeight-(innerHeight/10),longSide/30)
     paddle=new Paddle()
-    frameRate(12)
-    edges.push(new Sprite(width+width/2,height/2,width,height*2),new Sprite(0-width/2,height/2,width,height*2),new Sprite(width/2,0-height/2,width*2,height))
-    edges.forEach(edge => {
-        edge.collider='static'
-    });
+    frameRate(20)
+    edges=new Group()
+    new edges.Sprite(width+width/2,height/2,width,height*2,'static')
+    new edges.Sprite(0-width/2,height/2,width,height*2,'static')
+    new edges.Sprite(width/2,0-height/2,width*2,height,'static')
 }
-
+let frames=0
 var draw=()=>{
+    rectMode('center')
     clear()
     background(64)
     paddle.draw()
@@ -32,4 +33,9 @@ var draw=()=>{
             balls[0].start()
         }
     }
+    /*if(frames%10==0&&gameState==1){
+        let temp=new Ball(innerWidth/2, innerHeight-(innerHeight/10),longSide/30)
+        temp.start()
+    }*/
+    frames+=1
 }
